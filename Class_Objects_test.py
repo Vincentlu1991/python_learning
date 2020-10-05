1668ccc0bbc8d947e350f5d85c2114055185e7dd
Vincentlu1991 <luweixiong_v@hotmail.com>
2020-10-05 23:51:05 +0800

    new files

---
+++ Class_Objects_test.py
@@ -0,0 +1,9 @@
+from Class_Objects import Student
+
+# create a object of student
+student1 = Student('Jason', 'IT', 3.1, False)
+
+print(student1.name)
+print(student1.gpa)
+print(student1.major)
+print(student1.is_on_probation)
