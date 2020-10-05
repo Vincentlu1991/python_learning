0851cbe72c45a3e3e30ee604ec72804e5db4d5bb
Vincentlu1991 <luweixiong_v@hotmail.com>
2020-10-05 23:14:21 +0800

    Update Modules_and_Pip_test.py

--- Modules_and_Pip_test.py
+++ Modules_and_Pip_test.py
@@ -4,3 +4,9 @@ print(Modules_and_Pip.roll_dice(6))
 
 #Python Module Index
 #https://docs.python.org/3/py-modindex.html
+#search for python-docx
+
+#pip install python-docx
+#pip 是package manager，装modules用的
+#已经在windows 10上装了pip
+
